117ea84b56ac15f3f6446922b331360e6321a80d
Emanuele Disco <emawind84@gmail.com>
2017-08-31 17:32:08 +0900

    update

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("SangAh")]
 [assembly: AssemblyProduct("PMIS Offline Archive")]
-[assembly: AssemblyCopyright("Copyright ©SangAh 2016")]
+[assembly: AssemblyCopyright("Copyright ©SangAh 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -33,4 +33,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyFileVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.1000")]
