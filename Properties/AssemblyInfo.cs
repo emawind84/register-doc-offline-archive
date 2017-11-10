d12ffafdff51bd309778eb61b22030a36ad96e18
Emanuele Disco <emawind84@gmail.com>
2017-11-10 11:11:22 +0900

    update

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Windows;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("PMIS Offline Archive")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("PMIS Offline Archive SNAPSHOT")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("SangAh")]
 [assembly: AssemblyProduct("PMIS Offline Archive")]
