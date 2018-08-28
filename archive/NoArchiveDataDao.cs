de5d8448aa2574d9685f5c2f74c5fb821091285d
Emanuele Disco <emawind84@gmail.com>
2018-08-28 17:53:11 +0900

    added a mock implementation fro archive module

---
+++ archive/NoArchiveDataDao.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pmis.archive
+{
+    class NoArchiveDataDao : IArchiveDataDao
+    {
+        public void DeleteArchive()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ImportArchiveData(Archive a)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Archive LoadArchive(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DataTable SearchArchive(Dictionary<string, object> criteria = null)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
